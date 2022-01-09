13e9bf3c7741a78de8b08085c5d18ee8c9eea4c1
Olaitan Awe <laitanawe@gmail.com>
2022-01-09 09:13:57 -0500

    Updated fasterqdump script

---
+++ scripts/fasterqdump_16s.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Load the singularity module into environment
+# module load singularity
+
+rdp_train_path="/opt/rdp/rdp_train_set_16.fa.gz"
+runid_file="~/github/data/16s_crc_accessions.txt"
+container="~/github/containers/16s.sif"
+fastq_path="~/github/data/16s_crc/fastqs_16s"
+cpus=12
+
+
+## DADA2 workflow:
+#1. Obtain Raw FASTQ files: fasterq-dump
+#cat $runid_file | xargs singularity exec $container fasterq-dump --outdir $fastq_path -e $cpus
+cat $runid_file | xargs fasterq-dump --outdir $fastq_path -e $cpus
+
+##2. DADA2 workflow
+
+
+#3.
+
+
+#4.
+
+
+#5.
+
+
+#6.
+
+
+#7.
