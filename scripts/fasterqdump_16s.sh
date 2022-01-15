7b6d92ec25cbb08d6a107edceea026de4ad3a7cb
Olaitan I. Awe <laitanawe@gmail.com>
2022-01-15 08:12:51 +0100

    Updated scripts

--- scripts/fasterqdump_16s.sh
+++ scripts/fasterqdump_16s.sh
@@ -1,33 +1,17 @@
 #!/usr/bin/env bash
 
-# Load the singularity module into environment
+# This script accepts accessions and pulls down the fastqs from the SRA
+# Dependencies: SRA-Toolkit must be installed in the environment first
+# If using singularity container, load the singularity module into environment
 # module load singularity
 
 rdp_train_path="/opt/rdp/rdp_train_set_16.fa.gz"
-runid_file="/home/laitanawe/github/Colorectal-Metagenomics/data/16s_crc_accessions.txt"
+runid_file="/home/user/github/Colorectal-Metagenomics/data/16s_crc_accessions.txt"
 container="~/github/containers/16s.sif"
-fastq_path="/home/laitanawe/github/data/16s_crc/fastqs_16s"
+fastq_path="/home/user/github/data/16s_crc/fastqs_16s"
 cpus=12
 
 
-## DADA2 workflow:
-#1. Obtain Raw FASTQ files: fasterq-dump
+#Obtain Raw FASTQ files: fasterq-dump
 #cat $runid_file | xargs singularity exec $container fasterq-dump --outdir $fastq_path -e $cpus
 cat $runid_file | xargs fasterq-dump --outdir $fastq_path -e $cpus
-
-##2. DADA2 workflow
-
-
-#3.
-
-
-#4.
-
-
-#5.
-
-
-#6.
-
-
-#7.
