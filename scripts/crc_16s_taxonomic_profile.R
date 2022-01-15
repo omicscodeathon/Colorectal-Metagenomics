d553407f6614345d74ee41a952fc6a0e6300c607
Olaitan I. Awe <laitanawe@gmail.com>
2022-01-15 08:14:20 +0100

    Updated scripts

--- scripts/crc_16s_taxonomic_profile.R
+++ scripts/crc_16s_taxonomic_profile.R
@@ -1,8 +1,8 @@
 ##16s rRNA-seq analysis of CRC metagenomes
 #Quality check: R, xml2, httpuv, latticeExtra, dada2
-fastq_path <- "/home/laitanawe/my_shared_data_folder/16s_crc/fastqs_16s"
+fastq_path <- "/home/user/my_shared_data_folder/16s_crc/fastqs_16s"
 rdp_train_path="/opt/rdp/rdp_train_set_16.fa.gz"
-setwd("/home/laitanawe/my_shared_data_folder/github/Colorectal-Metagenomics/data/")
+setwd("/home/user/my_shared_data_folder/github/Colorectal-Metagenomics/data/")
 list.files(path=fastq_path, pattern=".fastq")
 
 #Assign Sample Names:
